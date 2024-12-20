840e2159703859b46ee7ecbb2df6ceefd7fa1bf2
Philips413 <philips413@kakao.com>
2024-12-20 12:33:35 +0900

    feature:챗방을 만들도록 한다.

--- src/pages/lobby/lobbyPage.tsx
+++ src/pages/lobby/lobbyPage.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 type ChatRoom = {
@@ -13,8 +13,9 @@ export default function LobbyPage() {
     const [chatRooms, setChatRooms] = useState<any[]>([]);
     const [chatRoomName, setChatRoomName] = useState<string>('');
     const [chatLimit, setChatLimit] = useState<number>(10);
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
-    const createChatRoom = () => {
+    const createChatRoom = async () => {
         const getUser: any = localStorage.getItem('user');
         const user = JSON.parse(getUser);
         const room = {
@@ -22,11 +23,13 @@ export default function LobbyPage() {
             limit: chatLimit,
             leader: user.partId
         };
-        axios.post(`http://localhost:8080/chat/room`, room);
+        await axios.post(`http://localhost:8080/chat/room`, room);
+        setIsModalOpen(false);
+        loadChatRooms();
     }
 
-    const loadChatRooms = () => {
-        axios.get(`http://localhost:8080/chat/rooms`)
+    const loadChatRooms = async () => {
+        await axios.get(`http://localhost:8080/chat/rooms`)
             .then((response) => {
                 setChatRooms([...response.data]);
             });
@@ -47,15 +50,13 @@ export default function LobbyPage() {
     return (
         <>
             <div className="flex items-center justify-center min-h-screen text-center">
-
                 <button
                     type="button"
                     className="btn btn-primary"
-                    aria-haspopup="dialog"
-                    aria-expanded="false"
-                    aria-controls="basic-modal"
-                    data-overlay="#basic-modal"
-                > 방 만들기</button>
+                    onClick={() => setIsModalOpen(true)}
+                >
+                    방 만들기
+                </button>
 
                 {chatRooms.length === 0 && <h1>방이 없습니다.</h1>}
                 {chatRooms.map((room: ChatRoom) => (
@@ -65,68 +66,63 @@ export default function LobbyPage() {
                     </div>
                 ))}
 
-                <div id="basic-modal"
-                    className="overlay modal overlay-open:opacity-100 hidden"
-                    role="dialog"
-                    tabIndex={-1}
-                >
-                    <div className="modal-dialog overlay-open:opacity-100" id={"basic-modal"}>
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h3 className="modal-title">방 만들기</h3>
-                                <button
-                                    type="button"
-                                    className="btn btn-text btn-circle btn-sm absolute end-3 top-3"
-                                    aria-label="Close"
-                                    data-overlay="#basic-modal"
-                                >
-                                    <span className="icon-[tabler--x] size-4"></span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                <div className="w-96">
-                                    <label
-                                        className="label label-text"
-                                        htmlFor="defaultInput"
-                                    > 방이름 </label>
-                                    <input
-                                        type="text"
-                                        placeholder="라이어 게임 스타트!!"
-                                        className="input input-sm"
-                                        value={chatRoomName} onChange={changeRoomName}
-                                    />
+                {isModalOpen && (
+                    <div id="basic-modal" className="overlay modal overlay-open:opacity-100 open opened">
+                        <div className="modal-dialog overlay-open:opacity-100">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h3 className="modal-title">방 만들기</h3>
+                                    <button
+                                        type="button"
+                                        className="btn btn-text btn-circle btn-sm absolute end-3 top-3"
+                                        aria-label="Close"
+                                        onClick={() => setIsModalOpen(false)}
+                                    >
+                                        <span className="icon-[tabler--x] size-4"></span>
+                                    </button>
                                 </div>
-                                <div className="w-96">
-                                    <label className="label label-text" htmlFor="defaultInput"> 참여인원 </label>
-                                    <input
-                                        type="number"
-                                        placeholder="John Doe"
-                                        className="input input-sm"
-                                        id="limit"
-                                        value={chatLimit}
-                                        onChange={changeRoomLimit}
-                                    />
+                                <div className="modal-body">
+                                    <div className="w-96">
+                                        <label className="label label-text" htmlFor="defaultInput"> 방이름 </label>
+                                        <input
+                                            type="text"
+                                            placeholder="라이어 게임 스타트!!"
+                                            className="input input-sm"
+                                            value={chatRoomName} onChange={changeRoomName}
+                                        />
+                                    </div>
+                                    <div className="w-96">
+                                        <label className="label label-text" htmlFor="defaultInput"> 참여인원 </label>
+                                        <input
+                                            type="number"
+                                            placeholder="John Doe"
+                                            className="input input-sm"
+                                            id="limit"
+                                            value={chatLimit}
+                                            onChange={changeRoomLimit}
+                                        />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="modal-footer">
-                                <button
-                                    type="button"
-                                    className="btn btn-primary"
-                                    onClick={createChatRoom}
-                                >
-                                    만들기
-                                </button>
-                                <button
-                                    type="button"
-                                    className="btn btn-soft btn-secondary"
-                                    data-overlay="#basic-modal"
-                                >
-                                    나가기
-                                </button>
+                                <div className="modal-footer">
+                                    <button
+                                        type="button"
+                                        className="btn btn-primary"
+                                        onClick={createChatRoom}
+                                    >
+                                        만들기
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="btn btn-soft btn-secondary"
+                                        onClick={() => setIsModalOpen(false)}
+                                    >
+                                        나가기
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                )}
             </div>
         </>
     );
